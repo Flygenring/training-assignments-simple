d30b9506cd68e071e31a4daf5f479fc666e77fe4
Flygenring <jbf@natmus.dk>
2021-06-09 12:09:07 +0200

    Use validity check from Accounts

--- src/java/eu/sig/training/ch04/Accounts.java
+++ src/java/eu/sig/training/ch04/Accounts.java
@@ -9,9 +9,11 @@ public class Accounts {
     // tag::isValid[]
     public static boolean isValid(String number) {
         int sum = 0;
+
         for (int i = 0; i < number.length(); i++) {
-            sum = sum + (9 - i) * Character.getNumericValue(number.charAt(i));
+            sum += (9 - i) * Character.getNumericValue(number.charAt(i));
         }
+
         return sum % 11 == 0;
     }
     // end::isValid[]
